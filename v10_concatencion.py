e7cc4df1c2c22af00f7034a58eccd63c6c75fb75
Hendrick Rafael <inghendrickrafael>
2023-03-06 11:47:00 -0400

    Nuevo documento concatenacion

---
+++ v10_concatencion.py
@@ -0,0 +1,10 @@
+#concatenacion
+
+nombre = "Hendrik"
+apellido = "Rafael"
+web = "MiprimeraWeb.com"
+
+print(nombre + " " + apellido +" "+ web)
+print(f"{nombre} {apellido} - {web}")
+print("{} {} {}".format(nombre, apellido, web))
+print("mi nombre es {} mi apellido {} pagina web {}".format(nombre, apellido, web))
